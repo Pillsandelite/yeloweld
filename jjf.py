90bbae55a42bf4b66c4dc8dd9f572e1312b40a9a
Pillsandelite <ezeadannalyn@gmail.com>
2021-08-19 17:37:04 +0100

    first commit

---
+++ jjf.py
@@ -0,0 +1,6 @@
+# This program says hello and asks for my name.
+print('Hello, world!') 
+print('What is your name')
+myName = input('Eze Adanna Lynda')
+print('It is good to meet you'+ myName)
+print('It is good to meet you' + 'Eze Adanna Lynda')
